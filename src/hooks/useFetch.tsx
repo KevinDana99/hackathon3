45d258b82ecb2a878afd8ec389970d2d0b839948
Kevin <kevindana99@gmail.com>
2025-10-04 12:37:59 -0300

    added fetching hook useFetch

--- src/hooks/useFetch.tsx
+++ src/hooks/useFetch.tsx
@@ -1,19 +1,25 @@
-import React, { useState } from "react";
+import { useEffect, useState } from "react";
 
-const useFetch = () => {
+const useFetch = (url: RequestInfo, options?: RequestInit) => {
   const [data, setData] = useState<any>(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<null | unknown>(null);
-  const fetching = () => {
+  const fetching = async () => {
     try {
+      const req = await fetch(url, options);
+      const res = await req.json();
       setLoading(true);
+      setData(res);
     } catch (err) {
       setError(err);
     } finally {
-      setData();
       setLoading(false);
     }
   };
+
+  useEffect(() => {
+    fetching();
+  }, []);
 
   return { data, loading, error };
 };
