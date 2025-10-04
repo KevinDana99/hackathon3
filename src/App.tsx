3c14e85f98d6221bb03fc77db05029d1644646f8
Kevin <kevindana99@gmail.com>
2025-10-04 11:11:18 -0300

    added deasy ui to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [state, setState] = useState("hello world");
   return (
     <>
-      <h1 className="text-3xl font-bold underline bg-red-300">Hello world!</h1>
+      <h1 className="text-3xl font-bold bg-red-300">{state}</h1>
     </>
   );
 }
